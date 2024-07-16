14b87c45495bfb40d7c50acca34b002d4f17951a
lina chorna <alina1chernaya@gmail.com>
2024-07-16 22:23:14 +0200

    first commit

---
+++ Praktikum/Praktikum 6 JS/JS/script.js
@@ -0,0 +1,28 @@
+"use strict"; /* обов'язково!*/
+
+let userName;
+console.log (typeof userName);
+userName = "freelance";
+console.log(typeof userName);
+userName = 58;
+console.log(typeof userName);
+
+let users = "25";
+let admins = "10";
+console.log(users+admins);
+
+console.log(+users + +admins);
+console.log(Number(users) + Number(admins));
+
+let resultOne = 2-1*5;
+console.log(resultOne);
+
+let resultTwo = (2-1)*5;
+console.log(resultTwo);
+
+let resulThree = 2-1+5;
+console.log(resulThree);
+
+let addU=2;
+addU++;
+console.log(addU);
